1f4e84e1a70106790b02e0045a8d63044e850c2f
seannli <seannli@umich.edu>
2024-05-28 20:51:21 -0400

    resume

---
+++ js/script.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ResumeDownload = () => {
+  return (
+    <div>
+      <a href="resume/Sean_Li_Resume.pdf" download="Sean_Li_Resume.pdf">
+        Download My Resume
+      </a>
+    </div>
+  );
+};
+
+export default ResumeDownload;
