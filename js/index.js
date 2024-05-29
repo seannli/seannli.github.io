dd7aab9ae38a66791646bcef0d7042628955fd70
seannli <seannli@umich.edu>
2024-05-28 20:35:23 -0400

    react

--- js/index.js
+++ js/index.js
@@ -1,3 +1,33 @@
+import React from 'react';
+
+// Importing individual components
+import Header from './components/Header';
+import Hero from './components/Hero';
+import About from './components/About';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import Blog from './components/Blog';
+import Testimonials from './components/Testimonials';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Hero />
+      <About />
+      <Projects />
+      <Skills />
+      <Blog />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link')
 
